baecbb999dde3db94019929e164a886c8bfb5186
RSAencrypton <15622109524m@gmail.com>
2023-11-06 18:54:55 +1100

    完成了菜品管理中的添加和删除操作

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,12 +1,17 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -23,9 +28,11 @@ public class DishController {
         return Result.success();
     }
 
-    @GetMapping("/getlist")
-    public Result GetDishList(@RequestParam Integer id) {
-        return Result.success();
+    @GetMapping("/page")
+    @ApiOperation("分页查询菜品列表")
+    public Result<PageResult> GetDishList(DishPageQueryDTO item) {
+        PageResult pageResult = dishService.GetDishList(item);
+        return Result.success(pageResult);
     }
 
     @PutMapping("/update")
@@ -33,8 +40,10 @@ public class DishController {
         return Result.success();
     }
 
-    @DeleteMapping("/delete")
-    public Result DeleteDish(@RequestParam Integer id) {
+    @DeleteMapping
+    @ApiOperation("删除菜品")
+    public Result DeleteDish(@RequestParam List<Long> ids) {
+        dishService.DeleteDish(ids);
         return Result.success();
     }
 
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,16 +1,26 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
+import com.sky.exception.DeletionNotAllowedException;
 import com.sky.mapper.DishMapper;
 import com.sky.mapper.FlavourMapper;
+import com.sky.mapper.MealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -24,6 +34,9 @@ public class DishServiceImpl implements DishService {
 
     @Autowired
     private FlavourMapper flavourMapper;
+
+    @Autowired
+    private MealMapper mealMapper;
 
     @Transactional
     public void AddDish(DishDTO item) {
@@ -39,5 +52,34 @@ public class DishServiceImpl implements DishService {
             }
             flavourMapper.InsertAllFlavour(flavors);
         }
+    }
+
+    public PageResult GetDishList(DishPageQueryDTO item){
+        PageHelper.startPage(item.getPage(), item.getPageSize());
+        Page<DishVO> dishList = dishMapper.GetDishList(item);
+        return new PageResult(dishList.getTotal(), dishList.getResult());
+    }
+
+    @Transactional
+    public void DeleteDish(List<Long> ids) {
+        for (Long id : ids) {
+            Dish dish = dishMapper.FindDishById(id);
+            if (dish.getStatus().equals(StatusConstant.ENABLE)) {
+                throw new DeletionNotAllowedException(MessageConstant.DISH_ON_SALE);
+            }
+        }
+
+        List<Long> mealIds = mealMapper.getMealsByDishId(ids);
+        if (mealIds != null && mealIds.size() > 0) {
+            throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
+        }
+
+//        for (Long id : ids) {
+//            dishMapper.DeleteDish(id);
+//            flavourMapper.DeleteFlavourByDishId(id);
+//        }
+
+        dishMapper.DeleteDishs(ids);
+        flavourMapper.DeleteFlavourByDishIds(ids);
     }
 }
