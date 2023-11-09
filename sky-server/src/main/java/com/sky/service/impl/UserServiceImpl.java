663aebce506b272a477965f543b4aaf3bfd7f3c4
RSAencrypton <15622109524m@gmail.com>
2023-11-09 15:59:05 +1100

    实现了微信登录功能

---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,65 @@
+package com.sky.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.google.gson.JsonObject;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.exception.LoginFailedException;
+import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private static final String loginApi = "https://api.weixin.qq.com/sns/jscode2session";
+
+    @Autowired
+    private WeChatProperties weChatProperties;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User login(UserLoginDTO userloginDTO) {
+        Map<String, String> params = new HashMap<>();
+        params.put("appid", weChatProperties.getAppid());
+        params.put("secret", weChatProperties.getSecret());
+        params.put("js_code", userloginDTO.getCode());
+        params.put("grant_type", "authorization_code");
+        String res = HttpClientUtil.doGet(loginApi, params);
+
+        JSONObject obj = JSON.parseObject(res);
+        String openid = obj.getString("openid");
+        if (openid == null) {
+            throw new LoginFailedException(MessageConstant.LOGIN_FAILED);
+        }
+
+        User user = userMapper.HasUser(openid);
+        if (user == null) {
+            user = User.builder()
+                    .openid(openid)
+                    .createTime(LocalDateTime.now())
+                    .build();
+            userMapper.InsertUser(user);
+        }
+
+        return user;
+    }
+
+
+    public User register(UserLoginDTO userloginDTO) {
+        return null;
+    }
+}
