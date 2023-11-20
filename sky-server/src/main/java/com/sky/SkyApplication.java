dda5088ff7edf54106732bbe4196c558126a1ffb
RSAencrypton <15622109524m@gmail.com>
2023-11-20 18:12:40 +1100

    给项目接入了redis集群

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -1,9 +1,12 @@
 package com.sky;
 
+import io.lettuce.core.ReadFrom;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -17,4 +20,9 @@ public class SkyApplication {
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
     }
+//
+//    @Bean
+//    public LettuceClientConfigurationBuilderCustomizer customizer() {
+//        return builder -> builder.readFrom(ReadFrom.REPLICA_PREFERRED);
+//    }
 }
